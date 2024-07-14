87182209d1cfc7c582cf882c4992e5afa4ab5d59
tamerkat <mahad10taha@gmail.com>
2024-07-14 14:09:32 +0300

    add

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const handelar = require('./handelar');
 const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
 app.use(express.json())
 
+const handelar = require('./handelar.js');
 
 
-app.get('api/users',handelar.getUser());
-app.get('api/users/id',handelar.getUserById())
-app.post('api/users',handelar.postNewUser())
-app.delete('api/users/id',handelar.deleteUser())
-app.patch('api/users/:id',handelar.patchUser())
+app.get('/', (req, res)=>{
+    res.send('good!')
+})
+
 
 let port = 3000
-app.listen(port, ()=> {
+app.listen(3000, ()=> {
     console.log(`server is running on port ${port}`)
 })
+app.use("/api", handelar);
+
+
+module.exports.handelar = serverless(app)
--- handelar.js
+++ handelar.js
@@ -1,80 +1,98 @@
+const express = require("express")
+const router = express.Router(); 
+
+
 const users = [
-    { id: 1, name: "Ammar Yasser", email: "ammar@me.io" },
-    { id: 2, name: "Omar Yasser", email: "omar@me.io" },
-    { id: 3, name: "Mai Yasser", email: "mai@me.io" },
+  { id: 1, name: "Ammar Yasser", email: "ammar@me.io" },
+  { id: 2, name: "Omar Yasser", email: "omar@me.io" },
+  { id: 3, name: "Mai Yasser", email: "mai@me.io" },
 ];
+  
+router.get("/getall", async (req, res)=>{
+  res.status(200).json({
+    status: "success",
+    data: users,
+  })
+})
 
-
-exports.getUser = (req, res)=> {
+router.get("/getone", async (req, res)=>{
+  try {
+    const id = req.query.id;
+    const user = users.find((user)=> user.id === +id)
+    if (!user){
+      return res.status(404).json({
+        massage: 'not fount id user'
+      })
+    }
     res.status(200).json({
-        status: "success",
-        data: users,
-    });
-};
+      massage: "success",
+      data: user
+    })
+  } catch (error) {
+    res.status(404).json({
+      massage: 'fail, not valied'
+    })
+  }
+})
 
+router.post("/postdata", async (req, res)=>{
+  const newdata = req.body;
+  users.push(newdata)
+  if (!newdata){
+    res.status.json({
+      massage: "invaild data"
+    })
+  }
+  res.status(200).json({
+    massage: "success",
+    data: users
+  })
+})
 
-exports.getUserById = (req, res)=> {
-    const id = req.params.id;
-    const user = users.find((user)=> user.id === id);
-    
-    if (!user) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'not valied'
-        })
+router.delete("/deleteuser", async (req, res)=>{
+  try {
+    const id = req.query.id;
+    const user = users.find((user)=> user.id === +id)
+
+    users.splice(id, 1)
+    if(!user){
+      return res.status(404).json({
+        massage: "invaild data"
+      })
+    } 
+  } catch (error) {
+    return res.status(404).json({
+      massage: "invaild data"
+    })
+  }
+  res.status(200).json({
+    massage: 'success',
+    data: users
+  })
+})
+
+router.patch("/patch", async (req, res)=>{
+  try {
+    const id = req.query.id;
+    const update = req.body;
+    const user = users.find((user)=> user.id === +id);
+  
+    if(!user || !update){
+      res.status(404).json({massage: 'invaild data'})
     }
 
+    user.title = update.title;
+    user.author = update.author;
+  
     res.status(200).json({
-        status: 'success',
-        data: user
+      massage: "success",
+      data: user
     })
-}
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({massage:"intrenal server error"})
+  }
+})
 
 
-exports.postNewUser = (req, res)=> {
-    const newuser = req.body;
-    users.push(newuser);
-
-    res.status(200).json({
-        status: 'success',
-        data: users
-    })
-}
-
-
-exports.deleteUser = (req, res)=> {
-    const id = req.params.id;
-    const user = users.find((user)=> user.id === id);
-    if (!user) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'not valied'
-        })
-    }
-    users.splice(user, 1);
-    res.status(200).json({
-        status: 'success',
-        data: users
-    })
-}
-
-
-exports.patchUser = (req, res) =>{
-    const id = req.params.id;
-    const updates = req.body;
-
-    const user = users.find((user)=> user.id === id);
-    if (!user) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'not valied'
-        })
-    }
-    user.name = updates.name;
-    user.email = updates.email;
-
-    res.status(200).json({
-        status: 'success',
-        data: user
-    })
-}
+module.exports = router ;
